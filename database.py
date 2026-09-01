5413860f2ea27bd44b0e76f69ce8ebce359a431e
Sidd <ksiddartha16@gmail.com>
2026-09-01 19:09:53 +0530

    Added database configs and model shcema of database

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+
+
+engine= create_engine(SQLALCHEMY_DATABASE_URI,connect_args={"check_same_thread":False})
+#check_same_thread is cuz of FastAPI supports multiple threads.. so
+
+# sessionmaker → creates database session factory
+# autocommit=False → manually commit changes using db.commit()
+# autoflush=False → don't automatically flush pending changes
+# bind=engine → use our database engine
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
