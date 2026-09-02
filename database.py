03905b8df30967d2b082370ec9e6c125139fe25b
Sidd <ksiddartha16@gmail.com>
2026-09-02 10:27:21 +0530

    Get todos method added and SQL database linked with application

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI, Depends, HTTPException, Path
+from typing_extensions import Annotated
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
 import models
+from models import Todos
+from starlette import status
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency= Annotated[Session,Depends(get_db)]
+
+@app.get("/",status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency,todo_id: int = Path(gt=0)):
+    todo_model= db.query(Todos).filter(Todos.id == todo_id).first()  # .first() to save and enhance performance
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
 
